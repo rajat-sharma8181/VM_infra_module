3032f22835dab71cecbee6a488c726bd4f34e21a
rajat-sharma8181 <rajats12120@gmail.com>
2025-11-17 09:52:17 +0530

    testing variable screts

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -3,48 +3,48 @@ module "rgmod" {
     rgs = var.rg_dev
 }
 
-module "netmod" {
-    depends_on = [ module.rgmod ]
-    source = "../../modules/azurerm_networking"
-    vnet = var.net_dev
-}
+# module "netmod" {
+#     depends_on = [ module.rgmod ]
+#     source = "../../modules/azurerm_networking"
+#     vnet = var.net_dev
+# }
 
-module "pipmod" {
-    depends_on = [ module.rgmod ]
-    source = "../../modules/azurerm_public_ip"
-    pip = var.pip_dev
-}
+# module "pipmod" {
+#     depends_on = [ module.rgmod ]
+#     source = "../../modules/azurerm_public_ip"
+#     pip = var.pip_dev
+# }
 
-module "nsgmod" {
-    depends_on = [ module.rgmod ]
-    source = "../../modules/azurerm_nsg"
-    nsg = var.nsg_dev
-}
+# module "nsgmod" {
+#     depends_on = [ module.rgmod ]
+#     source = "../../modules/azurerm_nsg"
+#     nsg = var.nsg_dev
+# }
 
-module "vmmod" {
-    depends_on = [ module.netmod ]
-    source = "../../modules/azurerm_compute"
-    vms = var.vm_dev
-}
+# module "vmmod" {
+#     depends_on = [ module.netmod ]
+#     source = "../../modules/azurerm_compute"
+#     vms = var.vm_dev
+# }
 
-module "mssqlservermod" {
-    depends_on = [ module.rgmod ]
-    source = "../../modules/azurerm_mssql_server"
-    mssql_server = var.mssql_server_dev
-}
+# module "mssqlservermod" {
+#     depends_on = [ module.rgmod ]
+#     source = "../../modules/azurerm_mssql_server"
+#     mssql_server = var.mssql_server_dev
+# }
 
-module "mssqldbmod" {
-    depends_on = [ module.rgmod, module.mssqlservermod ]
-  source = "../../modules/azurerm_mssql_database"
-  mssql_db = var.mssql_db_dev
-}
+# module "mssqldbmod" {
+#     depends_on = [ module.rgmod, module.mssqlservermod ]
+#   source = "../../modules/azurerm_mssql_database"
+#   mssql_db = var.mssql_db_dev
+# }
 
-module "baistianmod" {
-    depends_on = [ module.rgmod, module.netmod, module.pipmod ]
-    source = "../../modules/azurerm_bastion"
-    jump-host = var.bastion_dev
+# module "baistianmod" {
+#     depends_on = [ module.rgmod, module.netmod, module.pipmod ]
+#     source = "../../modules/azurerm_bastion"
+#     jump-host = var.bastion_dev
 
-}
+# }
   
 
 
